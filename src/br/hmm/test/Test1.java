497e9876542376d9a77393e38f07f8223c98f9ba
Marcelo Trier <marcelo.trier@gmail.com>
2014-07-10 15:36:33 -0300

    Pequenas alterações.

--- src/br/hmm/test/Test1.java
+++ src/br/hmm/test/Test1.java
@@ -36,7 +36,8 @@ public class Test1 {
 		}
 	}
 	
-	static int observacoes[] = { 1, 1, 1, 3, 3, 2, 1, 4, 4, 4, 1, 2, 3 };
+	static int observacoes[] = { 4, 4, 4, 3, 4, 3, 3, 3, 2, 2, 1 };
+	//static int observacoes[] = { 1, 1, 1, 3, 3, 2, 1, 4, 4, 4, 1, 2, 3 };
 	
 	public static void main(String[] args) {
 		Viterbi v = new Viterbi();
